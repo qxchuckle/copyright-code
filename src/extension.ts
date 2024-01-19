1289002678ba480ef897b48f330222da7722f983
qxchuckle <1934009145@qq.com>
2024-01-19 15:36:47 +0800

    中文本地化

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,31 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('copyright-code.extractCode', async () => {
-		// 获取当前工作区的根目录
-		const rootPath = await utils.getRootPath();
-		if (!rootPath) { return; }
-		// 获取需要提取的文件
-		const filesToExtract = await findFile(rootPath);
-		if (!filesToExtract) { return; }
-		// 提取并写入数据
-		await extractAndWriteData(rootPath, filesToExtract);
+	// 命令
+	const extractCodeCommand = vscode.commands.registerCommand('copyright-code.extractCode', async () => {
+		await extractCode();
 	});
-
-	context.subscriptions.push(disposable);
+	// 中文版命令
+	const extractCodeZHCommand = vscode.commands.registerCommand('copyright-code.extractCodeZH', async () => {
+		await extractCode();
+	});
+	// 注册命令
+	context.subscriptions.push(extractCodeCommand, extractCodeZHCommand);
 }
 
 export function deactivate() { }
+
+// 提取代码
+async function extractCode() {
+	// 获取当前工作区的根目录
+	const rootPath = await utils.getRootPath();
+	if (!rootPath) { return; }
+	// 获取需要提取的文件
+	const filesToExtract = await findFile(rootPath);
+	if (!filesToExtract) { return; }
+	// 提取并写入数据
+	await extractAndWriteData(rootPath, filesToExtract);
+}
 
 // 提取并写入数据
 async function extractAndWriteData(rootPath: string, filesToExtract: vscode.Uri[]) {
